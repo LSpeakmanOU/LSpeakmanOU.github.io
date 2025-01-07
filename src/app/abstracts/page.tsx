ca5b651072d2a891ab8f69b27996d4db5d59da2d
Liam Speakman <ls173818@ohio.edu>
2025-01-06 19:16:46 -0500

    Added Abstracts page

--- src/app/abstracts/page.tsx
+++ src/app/abstracts/page.tsx
@@ -1,9 +1,15 @@
 import Navigation from "@/components/navigation";
+import Abstract from "@/components/abstracts";
 
 
 
-export default function Home() {
+export default function Abstracts() {
   return (<div>
-      <Navigation title="Home"/>
+      <Navigation title="Abstracts"/>
+      
+      <div style={{marginLeft:"20px", marginRight:"20px"}}>
+      <h3>Conference Abstracts</h3>
+      <Abstract first="" bolded="Liam Speakman," last="Dominika Vojtasova, Michael Cooney, Christoph Thieme, Alexander Kukalev, Dario Righelli, Dominik Szabó, Ibai Irastorza-Azcarate, Elizabeth Medina, Kaitlyn Ford, Kristan Singletary, Izabela Harabula, Davide Risso, Lucia Peixoto, Ana Pombo, Lonnie Welch (2024) Discovering transcription factors driving changes in 3D genome architecture in glutamatergic neurons upon sleep deprivation. 2024 4D Nucleome Annual Meeting"/>
+      </div>
     </div>);
 }
